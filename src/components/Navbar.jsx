3a9e6acfbefbc65ef15ff22749edd00096939449
Om Patel <ompatel2019@gmail.com>
2024-09-10 21:55:57 +1000

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import logo from '../images/logoDark.svg';
-import useFadeIn from './Animations.jsx';
+import useFadeIn from './Animations.jsx'; // Import the custom fade-in hook
 import Redirect from './Redirect'; // Import the Redirect component
 
-const Navbar = () => {
-    useFadeIn();
+const Navbar = () => {  
+    useFadeIn(); // Apply the fade-in hook
 
     return (
         <>
             <div id='navbar' className='bg-c1 sticky top-0 z-50'>
                 <nav className='max-md:hidden 2xl:px-[160px] xl:px-[96px] lg:px-[72px] md:px-[24px] sm:px-[16px] max-sm:px-[16px] py-[32px] pt-[40px] max-sm:py-[24px] flex justify-between items-center bg-c1'>
-                    <div className="group">
-                        <Redirect to="home">
+                    <div>
+                        <div className="group">
                             <img className="w-16 max-sm:w-12 fade-left rotator" src={logo} alt="Bytesite digital logo" />
-                        </Redirect>
+                        </div>
                     </div>
+
                     <div className='flex space-x-4 items-center'>
                         <div className='bg-c2 2xl:text-xl text-c3 text-lg border-[1px] border-white px-8 p-[0.75rem] rounded-full fade-right'>
                             <ul className='font-questrial flex space-x-8'>
@@ -22,6 +23,7 @@ const Navbar = () => {
                                     <li className={`nav-item fadeIn-${index + 1}`} key={index}>
                                         <Redirect to={item.toLowerCase()}>
                                             <span className='nav-text-one'>{item}</span>
+                                            <span className='nav-text-two'>{item}</span>
                                         </Redirect>
                                     </li>
                                 ))}
